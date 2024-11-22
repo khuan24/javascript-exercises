9c5d919a2daf6a7779e0780c58a4c2827e3e0e92
Kexin Huang <khuan24@uwo.ca>
2024-11-22 16:33:26 -0500

    Complete 03, 04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(myString) {
+    let newString = ""
 
+    for (let i=myString.length; i>=0; i--) {
+        newString += myString.charAt(i)
+    }
+
+    return newString
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,30 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, ...args) {
+    // Naive approach without utilizing pre-existing array methods
+    // let newArray = []
+
+    // if (arguments.length < 2) {
+    //     return "ERROR"
+    // }
+
+    // for (let i=0; i<myArray.length; i++) {
+    //     let isMatch = false
+
+    //     for (let j=1; j<arguments.length; j++) {
+    //         if (myArray[i] === arguments[j]) {
+    //             isMatch = true
+    //         }
+    //     }
+        
+    //     if (!isMatch) {
+    //         newArray.push(myArray[i])
+    //     }
+        
+    // }
+    // return newArray
+
+    // Use includes() and filter()
+    // Filter myArray on a function: the element cannot be included in the list of elements to remove
+    return myArray.filter((element) => !args.includes(element))
 
 };
 
