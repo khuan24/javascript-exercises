d3947b6e2638397a22ee0291325f520179ebf555
Kexin Huang <khuan24@uwo.ca>
2024-11-22 15:38:50 -0500

    Complete 01, 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(myString, numTimes) {
+    if (numTimes < 0) {
+        return "ERROR"
+    }
+    
+    let newString = ""
+    for (let i=1; i<=numTimes; i++) {
+        newString += myString
+    }
 
+    return newString
 };
 
 // Do not edit below this line
