accaa234aa608454ee9b6e42c62d980931313020
Kexin Huang <khuan24@uwo.ca>
2024-11-22 17:43:26 -0500

    Complete 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  return roundNum(5*(tempF-32)/9)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  return roundNum(tempC*9/5+32)
 };
+
+const roundNum = function(num) {
+  return Math.round(num * 10) / 10
+}
 
 // Do not edit below this line
 module.exports = {
