8155b6554f359963fe12ab4410dc903707ab6a14
Kexin Huang <khuan24@uwo.ca>
2024-11-22 16:54:40 -0500

    Complete 05, 06

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    let sum = 0
 
+    // catch errors: can also use Number.isInteger()
+    if (typeof startNum != "number" || typeof endNum != "number") {
+        return 'ERROR'
+    } else if (startNum < 0 || endNum < 0) {
+        return 'ERROR'
+    }
+
+    // swap start and end if start is larger than end
+    if (startNum > endNum) {
+        let temp = startNum
+        startNum = endNum
+        endNum = temp
+    }
+
+    // calculate the sum
+    for (let i=startNum; i<=endNum; i++) {
+        sum += i
+    }
+
+    return sum
 };
 
 // Do not edit below this line
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    
+    if (year % 100 == 0 && year % 400 != 0) { // a year is not a leap year if it is divisible by 100, but not 400
+        return false
+    } else if (year % 4 == 0) { // leaps years are divisible by 4
+        return true
+    } else {
+        return false
+    }
+    
 };
 
 // Do not edit below this line
