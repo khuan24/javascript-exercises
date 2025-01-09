6ab7490ba11d2911788ae77a58adebae89c1df2e
Kexin Huang <khuan24@uwo.ca>
2025-01-09 12:08:21 -0500

    Complete palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // convert to lowercase , remove spaces and punctuation
+    // let processedStr = str.toLowerCase().replaceAll(" ", "").replace(/[.,?!"':;-]/g, "")
 
+    // or just replace every character that is not a lowercase letter
+    const processedStr = str.toLowerCase().replace(/[^a-z]/g, "")
+    // reverse and check if equal
+    return processedStr.split("").reverse().join("") === processedStr
 };
 
 // Do not edit below this line
