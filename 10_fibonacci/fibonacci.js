134d1443c467c4e49c49d895717d5f145db27814
Kexin Huang <khuan24@uwo.ca>
2025-01-09 12:31:01 -0500

    Complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(pos) {
+    // let pos = Number(num)
 
+    if (pos < 0) {
+        return "OOPS"
+    } else if (pos == 0) {
+        return 0
+    }
+    
+    let fib = []
+
+    for (let i=0; i<pos; i++) {
+        if (i == 0 || i == 1) {
+            fib.push(1)
+        } else {
+            fib.push(fib[i-1] + fib[i-2])
+        }
+    }
+
+    return fib[pos-1]
 };
 
 // Do not edit below this line
