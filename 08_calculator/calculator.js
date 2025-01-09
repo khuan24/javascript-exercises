4d1a30b64432ea2137b2d53cec26df7783fb09aa
Kexin Huang <khuan24@uwo.ca>
2025-01-09 11:37:58 -0500

    Complete calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, item) => sum + item, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, item) => product * item)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
-};
+function factorialRecursive(num) {
+  if (num == 0) {
+    return 1
+  } else if (num < 0) {
+    return -1
+  } else {
+    return num * factorialRecursive(num-1)
+  }
+}
+
+const factorial = factorialRecursive;
 
 // Do not edit below this line
 module.exports = {
