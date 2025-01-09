eb0c923b148c5e4e96dbc0148cb76152eb2f8273
Kexin Huang <khuan24@uwo.ca>
2025-01-09 12:53:58 -0500

    Complete findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    return arr.sort((a, b) => {
+        let aLastYear = a.yearOfDeath
+        let bLastYear = b.yearOfDeath
+        
+        if (!a.yearOfDeath) {
+            aLastYear = (new Date()).getFullYear()
+        } 
+
+        if (!b.yearOfDeath) {
+            bLastYear = (new Date()).getFullYear()
+        } 
+
+        return (bLastYear - b.yearOfBirth) - (aLastYear - a.yearOfBirth)
+    })[0]
 
 };
 
